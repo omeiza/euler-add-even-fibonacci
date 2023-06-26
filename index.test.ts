b36f6142660708c378d2c9d065f0605f2ef885b9
Omeiza Owuda <owudaomeiza@gmail.com>
2023-06-26 04:13:07 +0100

    Added limit to fib function execution in test

--- index.test.ts
+++ index.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test} from "@jest/globals";
 import fibAddEven from "./index";
 
 describe("Even numbers in a Fibonacci sequence of limit n", () => {
-	test("Get sum of even numbers in a Fibonacci from n to nth", () => {
+	test("Get sum of even numbers in a Fibonacci from 1 to 4000000", () => {
 		expect(fibAddEven(4000000)).toEqual(4613732);
 	});
 });
